2c35661c4a22d4076531a2f3dda85e6a689cd363
José Salcedo <josealberto3200@gmail.com>
2024-02-24 16:31:53 -0500

    Departamento - provincias arreglado

--- js/actas_ubigeo.js
+++ js/actas_ubigeo.js
@@ -14,35 +14,58 @@ const getDepartamentos = async(ubigeo)=>{
         html += `</select>`
         document.getElementById('departamentos').innerHTML=html
 
-        document.getElementById('provincias').innerHTML `
-            <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistritos(this.value) disabled;
+        document.getElementById('provincias').innerHTML = `
+            <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistritos(this.value) disabled>;
             <option selected="selected" value="">--SELECCIONE--</option>
         `
         html += `</select>`
     }
 }
 
+
 const getProvincias = async(departamento) =>{
     const data = await fetch(`https://oaemdl.es/onpe_sweb_php/actas/ubigeo/${ambito}/${departamento}`)
     if (data.status == 200){ 
         const provincias = await data.json()
         let html = `
-            <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistritos(this.value);
+            <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistritos(this.value)>;
                 <option selected="selected" value="">--SELECCIONE--</option>       
         `
         provincias.forEach(provincia => {
             html += `<option value="${provincia.Detalle}">${provincia.Detalle}</option>`
         });
-
         html += `</select>`
         document.getElementById('provincias').innerHTML=html
+
+        document.getElementById('distritos').innerHTML `
+            <select name="cdgoDist" id="cdgoDist" class="form-control" onchange="getLocales(this.value);
+                <option selected="selected" value="">--SELECCIONE--</option>
+        `
+        html += `</select>`
     }
 }
 
 
-/*const getDistritos
-<select name="cdgoDist" id="cdgoDist" class="form-control" onchange="
-    getPageWeb('', 'actas', 'getLocalesVotacion', 'divLocal', '&amp;tipoElec=&amp;ubigeo=' + this.value);
+const getDistritos = async(provincia) =>{
+    const data = await fetch(`https://oaemdl.es/onpe_sweb_php/actas/ubigeo/${ambito}/${departamento}/${provincia}`)
+    if(data.status == 200){
+        const distritos = await data.json()
+        let html = `
+            <select name="cdgoDist" id="cdgoDist" class="form-control" onchange="getLocales(this.value);
+                <option selected="selected" value="">--SELECCIONE--</option>
+        `
+        distritos.forEach(distrito => {
+            html += `
+                <option value="${distrito.Detalle}">${distrito.Detalle}</option>
+            `
+        });
+
+        html += `</select>`
+        document.getElementById('distritos').innerHTML=html
+    }
+}
+/*
+<select name="cdgoDist" id="cdgoDist" class="form-control" onchange="getPageWeb('', 'actas', 'getLocalesVotacion', 'divLocal', '&amp;tipoElec=&amp;ubigeo=' + this.value);
     getPageWeb('', 'actas', 'limpiarDiv', 'divDetalle', '');">
     <option selected="selected" value="">--SELECCIONE--</option>
     <option value="010202">ARAMANGO</option>
