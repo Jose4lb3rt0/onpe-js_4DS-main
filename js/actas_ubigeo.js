99b75005bbb9593950de58354ca0867f350c293f
José Salcedo <josealberto3200@gmail.com>
2024-02-24 18:20:45 -0500

    Borrador

--- js/actas_ubigeo.js
+++ js/actas_ubigeo.js
@@ -15,21 +15,22 @@ const getDepartamentos = async(ubigeo)=>{
         document.getElementById('departamentos').innerHTML=html
 
         document.getElementById('provincias').innerHTML = `
-            <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistritos(this.value) disabled>;
-            <option selected="selected" value="">--SELECCIONE--</option>
+            <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistritos(this.value);" disabled>;
+            <option selected="selected" value="" disabled>--SELECCIONE--</option>
         `
         html += `</select>`
     }
 }
 
-
+let departamentoElegido=''
 const getProvincias = async(departamento) =>{
+    departamentoElegido=departamento;
     const data = await fetch(`https://oaemdl.es/onpe_sweb_php/actas/ubigeo/${ambito}/${departamento}`)
     if (data.status == 200){ 
         const provincias = await data.json()
         let html = `
-            <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistritos(this.value)>;
-                <option selected="selected" value="">--SELECCIONE--</option>       
+            <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistritos(this.value);">;
+                <option selected="selected" value="">--SELECCIONE--</option>
         `
         provincias.forEach(provincia => {
             html += `<option value="${provincia.Detalle}">${provincia.Detalle}</option>`
@@ -37,33 +38,51 @@ const getProvincias = async(departamento) =>{
         html += `</select>`
         document.getElementById('provincias').innerHTML=html
 
-        document.getElementById('distritos').innerHTML `
-            <select name="cdgoDist" id="cdgoDist" class="form-control" onchange="getLocales(this.value);
-                <option selected="selected" value="">--SELECCIONE--</option>
+        document.getElementById('distritos').innerHTML = `
+            <select name="cdgoDist" id="cdgoDist" class="form-control" onchange="getLocales(this.value);" disabled>;
+            <option selected="selected" value="" disabled>--SELECCIONE--</option>
         `
         html += `</select>`
     }
 }
 
-
-const getDistritos = async(provincia) =>{
+let distritoElegido=''
+const getDistritos = async (provincia) => {
+    distritoElegido=distrito;
     const data = await fetch(`https://oaemdl.es/onpe_sweb_php/actas/ubigeo/${ambito}/${departamento}/${provincia}`)
-    if(data.status == 200){
-        const distritos = await data.json()
+    if (data.status == 200) {
+        const distritos = await data.json();
         let html = `
-            <select name="cdgoDist" id="cdgoDist" class="form-control" onchange="getLocales(this.value);
+            <select name="cdgoDist" id="cdgoDist" class="form-control" onchange="getLocales(this.value);">
                 <option selected="selected" value="">--SELECCIONE--</option>
         `
-        distritos.forEach(distrito => {
-            html += `
-                <option value="${distrito.Detalle}">${distrito.Detalle}</option>
-            `
+        distritos.forEach(distrito=>{
+            html += `<option value="${distrito.Detalle}">${distrito.Detalle}</option>`
         });
-
         html += `</select>`
         document.getElementById('distritos').innerHTML=html
     }
 }
+
+const getLocales = async(distrito)=>{
+    const data = await fetch(`https://oaemdl.es/onpe_sweb_php/actas/ubigeo/${ambito}/${departamento}/${provincia}/${distrito}`)
+    if (data.status == 200){
+        const locales = await data.json();
+        let html = `
+        
+        `
+    }
+}
+/*
+        let html = `<select name="cdgoDist" id="cdgoDist" class="form-control">
+            <option selected="selected" value="">--SELECCIONE--</option>`;
+        distritos.forEach(distrito => {
+            html += `<option value="${distrito.Detalle}">${distrito.Detalle}</option>`;
+        });
+        html += `</select>`;
+        document.getElementById('distritos').innerHTML = html;
+    }
+};
 /*
 <select name="cdgoDist" id="cdgoDist" class="form-control" onchange="getPageWeb('', 'actas', 'getLocalesVotacion', 'divLocal', '&amp;tipoElec=&amp;ubigeo=' + this.value);
     getPageWeb('', 'actas', 'limpiarDiv', 'divDetalle', '');">
